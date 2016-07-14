d95b96d558cdf4337183e378c23f4ceac45b23ec
tmoroz <tmoroz@intellias.com>
2016-07-14 11:33:39 +0300

    added network2 class for working with cross-entropy cost function

--- src/test.py
+++ src/test.py
@@ -1,10 +1,18 @@
 import mnist_loader
 import network
 
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
 
+
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network.Network([784, 30, 10])
+# net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
+
+
+import network2
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net2 = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net2.large_weight_initializer()
+net2.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True)
 
 # import network
 # from mnist_load import MnistLoad
